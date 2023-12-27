86c2c0db94f8b4af6518fab485efb95ccffdccec
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-27 22:29:05 +0700

    style: basic table desgin for Order page

---
+++ src/pages/orders/InputField.jsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box';
+import { FormControl } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
+
+const BootstrapInput = styled(InputBase)(
+  ({ theme, width, textColor, borderColor }) => ({
+    '& .MuiInputBase-input': {
+      borderRadius: 4,
+      position: 'relative',
+      backgroundColor: 'primary.bg',
+      fontSize: '12px',
+      width: width,
+      transition: theme.transitions.create(['border-color', 'border']),
+      '&:focus': {
+        border: '1px solid',
+        borderColor: borderColor,
+      },
+    },
+    '& input': { color: textColor },
+  })
+);
+
+// eslint-disable-next-line react/prop-types
+const InputField = ({ width, textColor, borderColor, dfValue, id }) => {
+  return (
+    <Box sx={{}}>
+      <FormControl variant="standard" sx={{ mt: '1px' }}>
+        <BootstrapInput
+          defaultValue={dfValue ? dfValue : ''}
+          id={id ? id : ''}
+          width={width ? width : 'auto'}
+          textColor={textColor ? textColor : '#fff'}
+          borderColor={borderColor ? borderColor : '#fff'}
+        />
+      </FormControl>
+    </Box>
+  );
+};
+
+export default InputField;
