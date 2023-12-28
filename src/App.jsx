79412ec5c5b6848000e82b87e3f4f93a967fce53
DAMOCLOIDS <khoa27012000@gmail.com>
2023-12-28 23:54:38 +0700

    style: basic router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import { Box } from '@mui/material';
-import Order from './pages/orders/Order';
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import Order from '~/pages/orders/Order';
+import { Routes, Route } from 'react-router-dom';
+import ImportProducts from './pages/orders/ImportProducts/ImportProducts';
+import Delivery from './pages/orders/Delivery/Delivery';
+import PriceForCustomer from './pages/orders/PriceForCustomer/PriceForCustomer';
 
 const App = () => {
   return (
@@ -14,6 +18,12 @@ const App = () => {
     >
       <Header />
       <Order />
+      <Routes>
+        {/* in development */}
+        <Route path="/import-products" element={<ImportProducts />} />
+        <Route path="/ship-and-print" element={<Delivery />} />
+        <Route path="/price-for-customer" element={<PriceForCustomer />} />
+      </Routes>
       <Footer />
     </Box>
   );
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import './globals.css';
 import { CssBaseline } from '@mui/material';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material';
 import { theme } from './styles/theme';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CssVarsProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </CssVarsProvider>
+    <BrowserRouter>
+      <CssVarsProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </CssVarsProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
